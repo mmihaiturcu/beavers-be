b312b6b37d25c12660c1d175c510192c63e6a1b1
Mihai Turcu <mihai.turcu@cctech.ai>
2022-05-04 13:05:57 +0300

    Spoofed beavers.com to look more professional :)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const SERVER_PORT = 9420;
-export const FRONTEND_URL = "https://localhost:9090";
+// export const FRONTEND_URL = "https://localhost:9090";
+export const FRONTEND_URL = 'https://beavers.com:9090';
 export const BACKEND_URL = `https://localhost:${SERVER_PORT}`;
